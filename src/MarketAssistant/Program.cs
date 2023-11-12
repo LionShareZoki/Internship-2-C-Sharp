3cb40d2d7529cc695583d41a4a3a0b952966e0d6
LionShareZoki <pujic.zoran@gmail.com>
2023-11-12 14:54:39 +0100

    Add DisplayMenuAndPick method

--- src/MarketAssistant/Program.cs
+++ src/MarketAssistant/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello world");
+static int DisplayMenuAndPick(List<(int Id, string Name)> menuItems)
+{
+    while (true)
+    {
+        Console.Clear();
+        foreach (var menuItem in menuItems)
+        {
+            Console.WriteLine($"{menuItem.Id} {menuItem.Name}");
+        }
+        Console.Write("Select one of the options: ");
+
+        var userInput = Console.ReadLine();
+        int? parsedInput = null;
+
+        try
+        {
+            parsedInput = int.Parse(userInput);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine($"Invalid input: Cannot parse '{userInput}'.");
+            Console.ReadKey();
+            continue;
+        }
+
+        if (!menuItems.Any(item => item.Id == parsedInput))
+        {
+            Console.WriteLine($"Invalid input: Id {parsedInput} does not exist.");
+            Console.ReadKey();
+            continue;
+        }
+
+        return parsedInput.Value;
+    }
+
+}
