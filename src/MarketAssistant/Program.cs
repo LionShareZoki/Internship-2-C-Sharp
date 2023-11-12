d1c76c9b27b2738be5ce516912b35f61dcdd9510
LionShareZoki <pujic.zoran@gmail.com>
2023-11-12 22:18:40 +0100

    Add ProductsMenuAction Method

--- src/MarketAssistant/Program.cs
+++ src/MarketAssistant/Program.cs
@@ -12,6 +12,98 @@ var mainMenuItems = new List<(int Id, string Name)>
     (4, "Statistika"),
     (5, "Izlaz iz aplikacije")
 };
+static void ProductsMenu(Dictionary<string, (DateOnly DateOfExpiry, int AvailableAmount, int SoldAmount)>? products)
+{
+    var productMenuItems = new List<(int Id, string Name)>
+    {
+        (1, "Unos artikla"),
+        (2, "Brisanje artikla"),
+        (3, "Uredivanje artikla"),
+        (4, "Ispis artikla"),
+        (5, "Glavni izbornik")
+    };
+
+    switch (DisplayMenuAndPick(productMenuItems))
+    {
+        case 1:
+            AddProductAction(products);
+            break;
+        case 2:
+            DeleteProductAction(products);
+            break;
+        case 3:
+            Console.WriteLine("uredivanje");
+            break;
+        case 4:
+            Console.WriteLine("ispis");
+            break;
+        case 5:
+            Console.WriteLine("glavni izb");
+            break;
+
+    }
+
+    static void AddProductAction(Dictionary<string, (DateOnly DateOfExpiry, int AvailableAmount, int SoldAmount)>? products)
+    {
+
+
+        Console.WriteLine("Unesite ime proizvoda:");
+        var name = Console.ReadLine();
+
+        Console.WriteLine("Unesite datum isteka proizvoda (yyyy-MM-dd):");
+        if (!DateOnly.TryParse(Console.ReadLine(), out DateOnly dateOfExpiry))
+        {
+            Console.WriteLine("Invalid date format. Please use yyyy-MM-dd.");
+            return;
+        }
+
+        Console.WriteLine("Unesite dostupnu količinu proizvoda:");
+        if (!int.TryParse(Console.ReadLine(), out int availableAmount))
+        {
+            Console.WriteLine("Invalid input for available amount. Please enter a valid integer.");
+            return;
+        }
+
+        products.Add(name, (dateOfExpiry, availableAmount, 0));
+
+        Console.WriteLine($"{name} usješno je dodan u proizvode");
+       
+        Console.ReadLine();
+    }
+
+    static void DeleteProductAction(Dictionary<string, (DateOnly DateOfExpiry, int AvailableAmount, int SoldAmount)>? products)
+    {
+        var deleteProductMenuItems = new List<(int Id, string Name)>
+    {
+        (1, "Po imenu artikla"),
+        (2, "Sve one kojima je istekao datum trajanja"),
+    };
+
+        switch (DisplayMenuAndPick(deleteProductMenuItems))
+        {
+            case 1:
+                Console.WriteLine("Izaberite proizvod koji želite ukloniti");
+                var name = Console.ReadLine();
+
+                products.Remove(name);
+
+                Console.WriteLine($"{name} uspješno je uklonjen iz proizvoda");
+                break;
+            case 2:
+                DateOnly currentDate = DateOnly.FromDateTime(DateTime.Now);
+
+                foreach (var item in products)
+                {
+                    if(item.Value.DateOfExpiry <= currentDate) products.Remove(item.Key);
+                    
+                }
+                break;
+        }
+
+
+
+    }
+}
 static int DisplayMenuAndPick(List<(int Id, string Name)> menuItems)
 {
     while (true)
