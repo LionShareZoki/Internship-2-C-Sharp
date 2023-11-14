f3e7fd9ea40879dd0ad08e0093acc1b64973f739
LionShareZoki <pujic.zoran@gmail.com>
2023-11-14 14:16:18 +0100

    Implement Calculate TotalPrice

--- src/MarketAssistant/Program.cs
+++ src/MarketAssistant/Program.cs
@@ -677,6 +677,17 @@ static void BillsMenu(Dictionary<string, (DateOnly DateOfExpiry, int AvailableAm
 
 
     }
+    static decimal CalculateTotalPrice(List<(string ProductName, int Quantity, decimal Price)> items)
+    {
+        decimal total = 0;
+
+        foreach (var item in items)
+        {
+            total += item.Quantity * item.Price;
+        }
+
+        return total;
+    }
     }
 
 static int DisplayMenuAndPick(List<(int Id, string Name)> menuItems)
